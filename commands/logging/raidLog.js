9377405f90ee171a4ecf1e5618cc09223ad7016b
Ererej <erikeliassonjohansson@gmail.com>
2024-03-31 22:56:51 +0200

    added /raidlog

---
+++ commands/logging/raidLog.js
@@ -0,0 +1,81 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('raidlog')
+		.setDescription('create the format for logging raids!')
+        .addStringOption(option => 
+            option.setName('enemy division(s)')
+                .setDescription('Give the name of the enemy division(s) here!')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('map?')
+                .setDescription('Give the name of the map the raid was played on!')
+                .setRequired(true)
+        )
+        .addAttachmentOption(option => 
+            option.setName('resoult')
+                .setDescription('Paste in the picture of the resoult!')
+                .setRequired(true)
+        )
+        .addBooleanOption(option =>
+            option.setName('win?')
+                .setDescription('Did you win?')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('allys name')
+                .setDescription('Give the name of the ally divisions here!')
+                .setRequired(false)
+        )
+        .addAttachmentOption(option => 
+            option.setName('raidDiscutions')
+                .setDescription('The raid will be logged as an outside raid if you fill out this feild!')
+                .setRequired(false)
+        ),
+
+	async execute(interaction) {
+        await interaction.deferReply()
+		const embeded_error = new EmbedBuilder().setColor([255,0,0])
+		if (!interaction.member.roles.cache.some(role => role.id === '1212084406282358846') && !interaction.member.permissions.has(PermissionsBitField.Flags.ManageRoles || PermissionsBitField.Flags.Administrator)) {
+            embeded_error.setDescription("Insuficent permissions!")
+            await interaction.editReply({ embeds: [embeded_error]});
+		} else {
+        try {
+        const enemy_division = interaction.options.getString('enemy division(s)')
+        const map = interaction.options.getString('map?')
+        const resoult = interaction.options.getAttachment('resoult')
+        const win = interaction.options.getBoolean('win?')
+        const allys_name = interaction.options.getString('allys name')
+        const raid_discutions = interaction.options.getAttachment('raidDiscutions')
+        const time = new Date()
+        const date = `${time.getDate()}/${time.getMonth()+1}/${time.getFullYear()}`
+        if (!allys_name) {
+            allys_name = ""
+        }
+        let winner = ""
+        if (win) {
+            winner = interaction.guild.name + " " + allys_name
+        } else {    
+            winner = enemy_division
+        }
+        const sea_format_channel = await interaction.guild.channels.cache.find(i => i.id === '1212085346464964659')
+        sea_format_channel.send(`VVV <#980566115187048499> VVV`)
+        if (!raid_discutions) {
+            await sea_format_channel.send(`Division(s): ${interaction.guild.name} ${allys_name} VS ${enemy_division} \nVictory: ${winner}\nMap: ${map}\nDate: ${date}\nScreenshot: ${resoult.url}`);
+        
+        } else {
+            await sea_format_channel.send(`Division(s): ${interaction.guild.name + " " + allys_name}\nEnemy Group: ${enemy_division} \nResoult: ${winner}\nMap: ${map}\nDate: ${date}\nProof: ${resoult.url} \n${raid_discutions.url}\n <@186267447001612289>`);
+        }
+        const embedReply = new EmbedBuilder()
+        .setColor([0,255,0])
+        .setDescription("format succesfully logged!")
+        interaction.editReply({ embeds: [embedReply]})
+        } catch (error) {
+            const embededError = new EmbedBuilder()
+            .setColor([255,0,0])
+            .setDescription("logging failed!")
+            await interaction.editReply({ embeds: [embededError]})
+        }
+}}}
