f3a47b171526e67ed7151e19d3a6a1014bb8452c
Ererej <erikeliassonjohansson@gmail.com>
2024-04-19 08:59:48 +0200

    attempted database fix

--- dbObjects.js
+++ dbObjects.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('s53_production_db', 'u53_DQi6jdTuOv', '9b3LG@mss3@8AzdmkG9AwJ.E', {
-	host: '127.0.0.1',
+	host: 'localhost',
 	port: '3306',
 	dialect: 'mysql',
 	logging: false,
