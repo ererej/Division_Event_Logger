733fa985418914d01d038e6c9bb7e0b49177167c
Ererej <erikeliassonjohansson@gmail.com>
2024-04-22 13:41:36 +0200

    added massMove command

---
+++ commands/utility/massMove.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, MessageEmbed, Permissions, ChannelType } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('massmove')
+        .setDescription('move all users in your voice channel to another voice channel!')
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('Please input the channel you want to move the users to!')
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildVoice)
+            ),
+
+    async execute(interaction) {
+        await interaction.deferReply()
+		const embeded_error = new MessageEmbed().setColor([255,0,50])
+		if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES || Permissions.FLAGS.ADMINISTRATOR)) {
+            embeded_error.setDescription("Insuficent permissions!")
+            await interaction.editReply({ embeds: [embeded_error]});
+		} else if (interaction.member.voice.channel === undefined) {
+            embeded_error.setDescription("You must be in a voice channel to use this command!")
+            await interaction.editReply({ embeds: [embeded_error]});
+        } else {
+            const currentVoiceChannel = interaction.member.voice.channel
+            const targetVoiceChannel = interaction.options.getChannel('channel')
+            const members = currentVoiceChannel.members
+            members.forEach(member => {
+                member.voice.setChannel(targetVoiceChannel)
+            })
+            const embeded = new MessageEmbed().setColor([0,255,0])
+            embeded.setDescription(`Moved ${members.size} in the ${currentVoiceChannel} voice chat to the ${targetVoiceChannel} voice chat!`)
+            await interaction.editReply({ embeds: [embeded]});
+        }
+    }
+}
--- dbInit.js
+++ dbInit.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('s53_production_db', 'u53_DQi6jdTuOv', '9b3LG@mss3@8AzdmkG9AwJ.E', {
-	host: 'localhost',
+const sequelize = new Sequelize('s53_production_db', 'u53_jcAhI5EQK8', 'XHvZ8@uZ^WxqJ0jSKI4Kybxb', {
+	host: '172.8.0.1',
+	port: "3306",
 	dialect: 'mysql',
 	logging: false,
 });
