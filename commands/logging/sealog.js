24d26ac8a6ef32ea81f5ab641225b0476b8d30eb
Ererej <erikeliassonjohansson@gmail.com>
2024-06-03 15:19:09 +0200

    added channel links and implomented it in all affected commands

--- commands/logging/sealog.js
+++ commands/logging/sealog.js
@@ -33,36 +33,34 @@ module.exports = {
             embeded_error.setDescription("Insuficent permissions!")
             await interaction.editReply({ embeds: [embeded_error]});
 		} else {
-            const announcmentMessageLink = interaction.options.getString('announcemnt_link')
-            let announcmentChannel;
-            try {
-                announcmentChannel = await interaction.guild.channels.cache.find(i => i.id === announcmentMessageLink.split("/")[5])
-            const announcmentMessage = await announcmentChannel.messages.fetch(announcmentMessageLink.split("/")[6])
-            
-            const time = announcmentMessage.createdAt
-            const date = `${time.getDate()}/${time.getMonth()+1}/${time.getFullYear()}`
-            const wedge_picture = interaction.options.getAttachment('wedge_picture').url
-            let sea_format_channel;
-            switch (interaction.guild.id) {
-                case '1073682080380243998': //FAF
-                    sea_format_channel = await interaction.guild.channels.cache.find(i => i.id === '1241499412853817446')
-                    break;
-                case '1104945580142231673':
-                    sea_format_channel = await interaction.guild.channels.cache.find(i => i.id === '1119307508457144464')
-                    break;
-            }
-            await sea_format_channel.send({content: `Division: ${interaction.guild.name}\nLink: ${announcmentMessageLink} \nDate: ${date}\nScreenshot: `, files: [{ attachment: wedge_picture, name: 'wedge.png'}]});
-            const embedReply = new EmbedBuilder()
-            .setColor([0,255,0])
-            .setDescription("format succesfully logged!")
-            interaction.editReply({ embeds: [embedReply]})
-                .then(() => { setTimeout(() => { interaction.deleteReply() }, 5000)})
-            } catch (error) {
-                const embededError = new EmbedBuilder()
-                .setColor([255,0,0])
-                .setDescription("logging failed make sure the announcment message is in the same guild as where you started this interaction!")
-                await interaction.editReply({ embeds: [embededError]})
-            }
+        const announcmentMessageLink = interaction.options.getString('announcemnt_link')
+        let announcmentChannel;
+        try {
+            announcmentChannel = await interaction.guild.channels.cache.find(i => i.id === announcmentMessageLink.split("/")[5])
+        const announcmentMessage = await announcmentChannel.messages.fetch(announcmentMessageLink.split("/")[6])
+        
+        const time = announcmentMessage.createdAt
+        const date = `${time.getDate()}/${time.getMonth()+1}/${time.getFullYear()}`
+        const wedge_picture = interaction.options.getAttachment('wedge_picture').url
+        let sea_format_channel;
+        switch (interaction.guild.id) {
+            case '1073682080380243998': //FAF
+                sea_format_channel = await interaction.guild.channels.cache.find(i => i.id === '1241499412853817446')
+                break;
+            case '1104945580142231673':
+                sea_format_channel = await interaction.guild.channels.cache.find(i => i.id === '1119307508457144464')
+                break;
         }
-    }
-}
+        await sea_format_channel.send({content: `Division: ${interaction.guild.name}\nLink: ${announcmentMessageLink} \nDate: ${date}\nScreenshot: `, files: [{ attachment: wedge_picture, name: 'wedge.png'}]});
+        const embedReply = new EmbedBuilder()
+        .setColor([0,255,0])
+        .setDescription("format succesfully logged!")
+        interaction.editReply({ embeds: [embedReply]})
+            .then(() => { setTimeout(() => { interaction.deleteReply() }, 5000)})
+        } catch (error) {
+            const embededError = new EmbedBuilder()
+            .setColor([255,0,0])
+            .setDescription("logging failed make sure the announcment message is in the same guild as where you started this interaction!")
+            await interaction.editReply({ embeds: [embededError]})
+        }
+}}}
