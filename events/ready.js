9b3952144e4999ed530dae728b0e66d6c0792d04
Ererej <erikeliassonjohansson@gmail.com>
2024-06-03 23:56:21 +0200

    bot status "playing bermuda air base"

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType  } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
+		client.user.setPresence({ activities: [{ name: 'Playing Bermuda Air Base', type: ActivityType.Playing}], status: 'online' });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
