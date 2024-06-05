53b6561c1bf344ee072dc4aaf23d40ba4c60ba59
Ererej <erikeliassonjohansson@gmail.com>
2024-06-05 23:00:27 +0200

    added usersinvc command

---
+++ commands/utility/usersinvc.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const db = require("../../dbObjects.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('usersinvc')
+        .setDescription('lists all the people in your Voice Channel!'),
+
+    async execute(interaction) {
+        await interaction.deferReply() 
+        const vcList = new EmbedBuilder()
+        .setTitle('People in your Voice Channel:')
+        .setColor('pink')
+        const voiceChannel = interaction.member.voice.channel;
+        if (!voiceChannel) {
+            return interaction.editReply({content: "You need to be in a voice channel to use this command!"})
+        }
+        const members = voiceChannel.members;
+        let string = "";
+        let oneEmbedSent = false;
+        members.forEach(async member => {
+            if (string.length + member.id.length + 5 > 4096) {
+                vcList.setDescription(string)
+                if (!oneEmbedSent) {
+                    await interaction.editReply({embeds: [vcList]})
+                } else {
+                    await interaction.followUp({embeds: [vcList]})
+                }
+                string = ""
+                oneEmbedSent = true;
+                
+            }
+            string += "<@" + member.id + ">\n"
+        });
+        vcList.setDescription(string)
+        if (!oneEmbedSent) {
+            return interaction.editReply({embeds: [vcList]})
+        } else {
+            return interaction.followUp({embeds: [vcList]})
+        }
+    }
+}
