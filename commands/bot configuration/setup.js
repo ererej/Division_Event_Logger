37dded354282cd76d08a3ec3f3f7c539e12b79de
Ererej <erikeliassonjohansson@gmail.com>
2024-06-10 19:31:38 +0200

    setup null exp fix

--- commands/bot configuration/setup.js
+++ commands/bot configuration/setup.js
@@ -33,13 +33,13 @@ module.exports = {
                         
                         if (guild) {
                                 guild.group_id = interaction.options.getInteger("roblox_group_id")
-                                guild.exp = interaction.options.getInteger("current_exp")
+                                guild.exp = interaction.options.getInteger("current_exp") ? interaction.options.getInteger("current_exp") : guild.exp
                                 guild.name = interaction.options.getString("division_name") ? interaction.options.getString("division_name") : guild.name ? guild.name : interaction.guild.name
                                 await guild.save();
                                 const embeded_reply = new EmbedBuilder().setDescription("successfuly updated the linked group and the total exp").setColor([0,255,0])
                                 await interaction.editReply({ embeds: [embeded_reply]});
                         } else {
-                                await db.Servers.create({ guild_id: interaction.guild.id, group_id: interaction.options.getInteger("roblox_group_id") ? interaction.options.getInteger("roblox_group_id") : interaction.guild.name, exp: interaction.options.getInteger("current_exp")})
+                                await db.Servers.create({ guild_id: interaction.guild.id, group_id: interaction.options.getInteger("roblox_group_id") ? interaction.options.getInteger("roblox_group_id") : interaction.guild.name, exp: interaction.options.getInteger("current_exp") ? interaction.options.getInteger("current_exp") : 0})
                                 const embeded_reply = new EmbedBuilder().setDescription("server successfuly saved to the database and linked to the roblox group.").setColor([0,255,0])
                                 await interaction.editReply({ embeds: [embeded_reply]});
                         }
