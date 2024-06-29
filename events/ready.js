5de766b701a7f37fbfd5b7f0653beb70864a893b
Ererej <erikeliassonjohansson@gmail.com>
2024-06-29 23:41:29 +0200

    fix?2

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,20 @@
 const { Events, ActivityType, ChannelType } = require('discord.js');
-
+const config = require("../config.json")
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
-		client.guilds.cache.forEach(guild => {
-			let channels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildText);
-			guild.invites.create(channels.first().id).then(invite => console.log(`[READY] ${guild.name} has ${guild.memberCount} members! Invite:  https://discord.gg/` + invite.code))
-		});
+		if (config.host === "Laptopp") {
+			client.guilds.cache.forEach(guild => {
+				let channels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildText);
+				guild.invites.create(channels.first().id).then(invite => console.log(`[READY] ${guild.name} has ${guild.memberCount} members! Invite:  https://discord.gg/` + invite.code))
+			
+			});
+		} else {
+			client.guilds.cache.forEach(guild => {
+					console.log(`[READY] ${guild.name} has ${guild.memberCount} members!`)
+			});
+		}
 		client.user.setPresence({ activities: [{ name: client.guilds.cache.size + " divisions", type: ActivityType.Watching}], status: 'online' });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
