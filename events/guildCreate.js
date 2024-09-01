1689cc646ccbd03641f05847169b52e38490880f
Ererej <erikeliassonjohansson@gmail.com>
2024-09-01 11:37:23 +0200

    made /setup not return an error for new guilds

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -12,7 +12,7 @@ module.exports = {
         let channels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildText);
         console.log("test")
 		const invite = await guild.invites.create(channels.first().id)
-        console.log(`[GUILD JOIN] ${guild.name} which has  ${guild.memberCount} members! Invite:  https://discord.gg/` + invite.code)
+        console.log(`[GUILD JOIN] ${guild.name} which has  ${guild.memberCount} members!` /*Invite:  https://discord.gg/` + invite.code*/)
         console.log("hi")
         /*
         const testServer = client.guilds.cache.find(guild => guild.id == "831851819457052692")
