547466842393454c5cda955aceff2a02f8714882
Ererej <erikeliassonjohansson@gmail.com>
2024-10-04 14:36:05 +0200

    The DAs deleted the header for the divisions colum. So I had to make finding the exp work without knowing the name of the column

--- commands/botConfiguration/linkChannel.js
+++ commands/botConfiguration/linkChannel.js
@@ -102,13 +102,13 @@ module.exports = {
             const server = await db.Servers.findOne({ where: { guild_id: interaction.guild.id } })
             const division_name = server ? server.name : interaction.guild.name
             const sheetData = await parser.parse()
-            const row = sheetData.find(row => row["Divisions"] === division_name)
+
+            const firstColumnName = Object.keys(sheetData[0])[0]
+            const row = sheetData.find(row => row[firstColumnName] === division_name)
             if (!row) {
-                if (sheetData[0]["Divisions".trim()]) {
+                if (sheetData[0][firstColumnName]) {
                     return await interaction.editReply({ content: `could not locate the division: ${division_name} in the officer tracker!`, ephemeral: true})
-                } else {
-                    return await interaction.editReply({ content: `could not locate a column named "Divisions" in the officer tracker! This means that the DAs has made an opsie again! Please contact Ererej <@386838167506124800> so that he can scream at them!`})
-                }
+                } 
             }
             const exp = row.EXP.slice(10).trim()
             if (!exp) return await interaction.editReply({ content: `<#${dbChannel.channel_id}> was made the EXP DISPLAY channel but there was an error while fetching the exp! This is mostlikely due to your divisions name not being the same as your discord servers name. But it can also be due to your division needing to be in the officer tracker for this to work.`, ephemeral: true })
