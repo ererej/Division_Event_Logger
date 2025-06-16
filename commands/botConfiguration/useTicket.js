e4f480ab616f3b22e9b4f212524ea4935f1b8a84
Ererej <erikeliassonjohansson@gmail.com>
2025-06-16 13:51:52 +0200

    added more premium tickets and fixed a bug in /remove_all_ranks

--- commands/botConfiguration/useTicket.js
+++ commands/botConfiguration/useTicket.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, PermissionsBitField, Colors } = require('discord.js');
 const db = require("../../dbObjects.js")
 const noblox = require("noblox.js") 
 const config = require("../../config.json")
@@ -49,27 +49,81 @@ module.exports = {
             return await interaction.editReply({ embeds: [embeded_error]});
 		} 
 
-        const ticketPrices = {"1298023132027944980": 0.99, "1383014678002667571": 4.99}
-
+        const ticketPrices = {"1298023132027944980": 0.99, "1384130405560615002": 2.99, "1383014678002667571": 4.99}
+        
         let pricePerTicket = 0.99 
         const premiumRedeemLogsChannel = testingServer.channels.cache.get("1328990301565616250")
         if (interaction.options.getString('code') == null) {
 
-            const entitelments = (await interaction.client.application.entitlements.fetch()).filter(e => e.userId === interaction.user.id && (e.skuId === '1298023132027944980' || e.skuId === "1383014678002667571") && e.consumed === false)
+            const entitelments = (await interaction.client.application.entitlements.fetch()).filter(e => e.userId === interaction.user.id && (e.skuId === '1298023132027944980' || e.skuId === "1383014678002667571" || e.skuId === "1384130405560615002") && e.consumed === false)
+            console.log("Entitelments: ", entitelments)
             if (entitelments.size < 1) {
                 const premiumButton1 = new ButtonBuilder() // Button linking to the store page for premium tickets
                     .setStyle(6)
                     .setSKUId('1298023132027944980')
+                const premiumButton3 = new ButtonBuilder() // Button linking to the store page for premium tickets
+                    .setStyle(6)
+                    .setSKUId('1384130405560615002')
                 const premiumButton5 = new ButtonBuilder() // Button linking to the store page for premium tickets
                     .setStyle(6)
-                    .setSKUId('1298023132027944980')
-                const row = new ActionRowBuilder().addComponents([premiumButton1, premiumButton5])
+                    .setSKUId('1383014678002667571')
+                const row = new ActionRowBuilder().addComponents([premiumButton1, premiumButton3, premiumButton5])
 
                 return await interaction.editReply({ embeds: [embeded_error.setDescription("You dont have any premium tickets. You can buy one here")], components: [row]}) 
             }
-            
-            if (entitelments.length > 1) {
-                
+
+
+            let ticket = entitelments.first()
+            let pricePerTicket = ticketPrices[ticket.skuId]
+
+            if (entitelments.size > 1) {
+                const ticketRow = new ActionRowBuilder()
+                if (entitelments.find(e => e.skuId === "1298023132027944980")) {
+                    const ticketButton1 = new ButtonBuilder()
+                    .setStyle(1)
+                    .setCustomId('ticketButton1')
+                    .setLabel(`${ticketPrices["1298023132027944980"]}$`)
+                    ticketRow.addComponents([ticketButton1])
+                }
+                if (entitelments.find(e => e.skuId === "1384130405560615002")) {
+                    const ticketButton3 = new ButtonBuilder()
+                    .setStyle(1)
+                    .setCustomId('ticketButton3')
+                    .setLabel(`${ticketPrices["1384130405560615002"]}$`)
+                    ticketRow.addComponents([ticketButton3])
+                }
+                if (entitelments.find(e => e.skuId === "1383014678002667571")) {
+                    const ticketButton5 = new ButtonBuilder()
+                    .setStyle(1)
+                    .setCustomId('ticketButton5')
+                    .setLabel(`${ticketPrices["1383014678002667571"]}$`)
+                    ticketRow.addComponents(ticketButton5)
+                }
+                const responce = await interaction.editReply({ embeds: [new EmbedBuilder().setColor(Colors.DarkVividPink).setDescription(`You have ${entitelments.size} Tickets which one do you want to use?`)], components: [ticketRow]})
+                const collectorFilter = i => i.customId.startsWith("ticketButton") && i.user.id === interaction.user.id
+                try {
+                    const confirmation = await responce.awaitMessageComponent({ Filter: collectorFilter, time: 300_000 })
+                    confirmation.deferUpdate()
+                    if (confirmation.customId === 'ticketButton1') {
+                        pricePerTicket = ticketPrices["1298023132027944980"]
+                        ticket = entitelments.find(e => e.skuId === "1298023132027944980")
+
+                    } else if (confirmation.customId === 'ticketButton3') {
+                        pricePerTicket = ticketPrices["1384130405560615002"]
+                        ticket = entitelments.find(e => e.skuId === "1384130405560615002")
+
+                    } else if (confirmation.customId === 'ticketButton5') {
+                        pricePerTicket = ticketPrices["1383014678002667571"]
+                        ticket = entitelments.find(e => e.skuId === "1383014678002667571")
+                    }
+                    
+                } catch (error) {
+                    if (error.message === "Collector received no interactions before ending with reason: time") {
+                        return interaction.editReply({embeds: [embeded_error.setDescription("No responce was given in within 5 minutes, cancelling!")], components: []})
+                    } else {
+                            throw error
+                    }
+                }
             }
 
             const memberCount = interaction.guild.memberCount
@@ -87,8 +141,8 @@ module.exports = {
 
 
             await server.save()
-            await entitelments.first().consume()
-            await interaction.editReply({ embeds: [new EmbedBuilder().setDescription(`You have activated premium for ${Math.round(daysToAdd)} days! It will end in ${Math.round((server.premium_end_date - currentTime.getTime())/ (24 * 60 * 60 * 1000))} days. Price per month for your div is ${pricePerMounth}$ the price increses with 0.2$ for every 500 members you get\nYou have ${entitelments.size - 1} tickets left!\nThanks for supporting me and my bot!`).setColor([255, 105, 180])]})
+            await ticket.consume() // Consumes the ticket so it can't be used again
+            await interaction.editReply({ embeds: [new EmbedBuilder().setDescription(`You have activated premium for ${Math.round(daysToAdd)} days! It will end in ${Math.round((server.premium_end_date - currentTime.getTime())/ (24 * 60 * 60 * 1000))} days. Price per month for your div is ${pricePerMounth}$ the price increses with 0.2$ for every 500 members you get\nYou have ${entitelments.size - 1} tickets left!\nThanks for supporting me and my bot!`).setColor([255, 105, 180])], components: []})
 
             await premiumRedeemLogsChannel.send(`User: ${interaction.user} id: ${interaction.user.id} has used a ticket to activate premium for ${daysToAdd} days! \nPrice per month: ${pricePerMounth}$ \nGuild: ${interaction.guild.name} id: ${interaction.guild.id}!`)
 
