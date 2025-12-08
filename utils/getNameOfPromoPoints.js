22ff1c9b75f6cd970827448bf0d4016232717ceb
Ererej <erikeliassonjohansson@gmail.com>
2025-12-08 23:05:30 +0100

    reveted change to getNameOfPromoPoints

--- utils/getNameOfPromoPoints.js
+++ utils/getNameOfPromoPoints.js
@@ -3,7 +3,7 @@ module.exports = async (db, guildId, settings) => {
     
     const dbNameOfPromoPoints = settings ?
      await settings.findOne({ where: { guild_id: guildId, type: "nameofpromopoints" } }) 
-     : await db.settings.findOne({ where: { guild_id: guildId, type: "nameofpromopoints" } })
+     : await db.Settings.findOne({ where: { guild_id: guildId, type: "nameofpromopoints" } })
     if (dbNameOfPromoPoints) {
         nameOfPromoPoints = dbNameOfPromoPoints.config
     }
